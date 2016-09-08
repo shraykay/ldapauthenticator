6ebd43c4f46a93aa651189514f7c5293ab7db7ef
clongbottom <longbottom@gmail.com>
2016-09-08 15:54:59 +0100

    Change so that we login with admin user and get DN of user from mail attribute search, before trying the final bind on DN.

--- ldapauthenticator/ldapauthenticator.py
+++ ldapauthenticator/ldapauthenticator.py
@@ -41,6 +41,19 @@ class LDAPAuthenticator(Authenticator):
         """
     )
 
+    bind_user_template = Unicode(
+        config=True,
+        help="""
+        Template from which to construct the full dn
+        when authenticating to LDAP. {username} is replaced
+        with the actual username.
+
+        Example:
+
+            uid={username},ou=people,dc=wikimedia,dc=org
+        """
+    )
+
 
     allowed_groups = List(
         config=True,
@@ -48,7 +61,7 @@ class LDAPAuthenticator(Authenticator):
     )
 
     valid_username_regex = Unicode(
-        r'^[a-z][.a-z0-9_\-@]*[a-z]{2,}$',
+        r'^[a-z][.a-z0-9_\-\@]*[a-z]{2,}$',
         config=True,
         help="""Regex to use to validate usernames before sending to LDAP
 
@@ -83,6 +96,19 @@ class LDAPAuthenticator(Authenticator):
         """
     )
 
+    admin_user = Unicode(
+        r'admin',
+        config=True,
+        help="""Admin password
+        """
+    )
+    admin_password = Unicode(
+        r'admin',
+        config=True,
+        help="""Admin password
+        """
+    )
+
     @gen.coroutine
     def authenticate(self, handler, data):
         username = data['username']
@@ -98,50 +124,41 @@ class LDAPAuthenticator(Authenticator):
             self.log.warn('Empty password')
             return None
 
-        userdn = self.bind_dn_template.format(username=username)
+        admindn = self.bind_dn_template.format(username=admin_user)
 
         server = ldap3.Server(
             self.server_address,
             port=self.server_port,
             use_ssl=self.use_ssl
         )
-        conn = ldap3.Connection(server, user=userdn, password=password)
+
+        conn = ldap3.Connection(server, user=admindn, password=admin_password)
 
         if conn.bind():
-            if self.allowed_groups:
-                if self.lookup_dn:
-                    # In some cases, like AD, we don't bind with the DN, and need to discover it.
-                    conn.search(
-                        search_base=self.user_search_base,
-                        search_scope=ldap3.SUBTREE,
-                        search_filter='({userattr}={username})'.format(
-                            userattr=self.user_attribute,
-                            username=username
-                        ),
-                        attributes=[self.user_attribute]
-                    )
 
-                    if len(conn.response) == 0:
-                        self.log.warn('User with {userattr}={username} not found in directory'.format(
-                            userattr=self.user_attribute, username=username))
-                        return None
-                    userdn = conn.response[0]['dn']
+            conn.search(
+                search_base=self.user_search_base,
+                search_scope=ldap3.SUBTREE,
+                search_filter='({userattr}={username})'.format(
+                    userattr=self.user_attribute,
+                    username=username
+                ),
+                attributes=[self.user_attribute]
+            )
 
-                for group in self.allowed_groups:
-                    if conn.search(
-                        group,
-                        search_scope=ldap3.BASE,
-                        search_filter='(member={userdn})'.format(userdn=userdn),
-                        attributes=['member']
-                    ):
-                        return username
-                # If we reach here, then none of the groups matched
-                self.log.warn('User {username} not in any of the allowed groups'.format(
-                    username=userdn
-                ))
+            if len(conn.response) == 0:
+                self.log.warn('User with {userattr}={username} not found in directory'.format(
+                    userattr=self.user_attribute, username=username))
                 return None
-            else:
+            users_dn_attr = conn.response[0]['dn']
+
+            logindn = self.bind_user_template.format(username=users_dn_attr)
+
+            user_conn = ldap3.Connection(server, user=logindn, password=password)
+
+            if user_conn.bind():
                 return username
+
         else:
             self.log.warn('Invalid password for user {username}'.format(
                 username=userdn,
